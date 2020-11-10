086b10b62cb2e95f0729204accbf274bf3181e37
voontv <voontv@gmail.com>
2020-11-10 08:20:48 +0700

    first project

---
+++ LearnVB/Exercise1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Exercise1
+    Function UCLN(ByVal num1 As Integer, ByVal num2 As Integer) As Long
+
+        Dim result As Long = 1
+        If (num1 = 0) Or (num2 = 0) Then
+            result = 0
+        ElseIf ((num1 Mod num2) = 0) Then
+            result = num2
+        ElseIf ((num2 Mod num1) = 0) Then
+            result = num1
+        Else
+            Dim i As Integer
+            If num1 > num2 Then
+                i = num1 - num2
+            Else
+                i = num2 - num1
+            End If
+            For j As Integer = i To 1 Step -1
+                If (num1 Mod j) = 0 And (num2 Mod j) = 0 Then
+                    result = j
+                    Exit For
+                End If
+            Next
+        End If
+
+        Return result
+
+    End Function
+
+    Function BCNN(ByVal num1 As Integer, ByVal num2 As Integer) As Long
+        If (num1 * num2 = 0) Then
+            Return 0
+        ElseIf UCLN(num1, num2) = 1 Then
+            Return num1 * num2
+        ElseIf num1 > num2 Then
+            Return num2 * UCLN(num1, num2)
+        Else
+            Return num1 * UCLN(num1, num2)
+        End If
+    End Function
+End Class
---
+++ LearnVB/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim exercise1 As New Exercise1()
+        Console.WriteLine(" Uoc chung lon nhat la {0} ", exercise1.UCLN(17, 20))
+        Console.WriteLine(" Boi chung nho nhat la {0} ", exercise1.BCNN(17, 20))
+    End Sub
+End Module
